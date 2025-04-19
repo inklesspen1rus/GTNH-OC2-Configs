657cece52f4c1fb1acb7c29c267234e6e51ebc95
Uchuvadov Stanislav <inklesspen@yandex.ru>
2025-04-20 02:43:47 +0300

    Fixed config

--- ae2-netrunner.lua
+++ ae2-netrunner.lua
@@ -1,7 +1,7 @@
 return {
     command = './ae2-level.lua serve ae2-level-config.lua',
     files = {
-        ['ae2-level.lua'] = 'https://github.com/inklesspen1rus/GTNH-OC-Scripts/blob/main/bin/ae2-level.lua',
-        ['ae2-level-config.lua'] = 'https://github.com/inklesspen1rus/GTNH-OC2-Configs/blob/main/bin/ae2-level-config.lua',
+        ['ae2-level.lua'] = 'https://raw.githubusercontent.com/inklesspen1rus/GTNH-OC-Scripts/refs/heads/main/bin/ae2-level.lua',
+        ['ae2-level-config.lua'] = 'https://raw.githubusercontent.com/inklesspen1rus/GTNH-OC2-Configs/refs/heads/main/ae2-level-config.lua',
     }
 }
