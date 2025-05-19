d0c0a0edad935b257f74d0b41d21c8965f86b7d0
Uchuvadov Stanislav <inklesspen@yandex.ru>
2025-05-19 12:29:50 +0300

    Add graphene in stock

--- ae2-level-config.lua
+++ ae2-level-config.lua
@@ -28,6 +28,7 @@ return
         { level = 10000, batch = 1, selector = { label = 'Rutile Dust' } },
         { level = 1024, batch = 1, selector = { label = 'Platinum Dust' } },
         { level = 1024, batch = 1, selector = { label = 'Iridium Dust' } },
+        { level = 1024, batch = 1, selector = { label = 'Graphene Dust' } },
         { level = 16, batch = 16, selector = { label = 'Rhodium Nitrate Dust' } },
         { level = 1024, batch = 1, selector = { label = 'Rhodium Dust' } },
         { level = 512, batch = 8, selector = { label = 'Silicon Solar Grade (Poly SI) Dust' } },
